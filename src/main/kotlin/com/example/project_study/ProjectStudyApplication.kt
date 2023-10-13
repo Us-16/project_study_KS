cb463acde08f47c344e7a89b7d38405cccd4ea9f
JorimJoram <wkdgyfla97@gmail.com>
2023-10-13 10:17:36 +0900

    start init

---
+++ src/main/kotlin/com/example/project_study/ProjectStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.project_study
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectStudyApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProjectStudyApplication>(*args)
+}
