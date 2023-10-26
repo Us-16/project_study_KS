6722f188e189c3a89d3e97226946a3c573ff3a15
JorimJoram <wkdgyfla97@gmail.com>
2023-10-26 17:24:37 +0900

    test: response object to JSON

--- src/main/kotlin/com/example/project_study/config/session_security/CustomLoginSuccessHandler.kt
+++ src/main/kotlin/com/example/project_study/config/session_security/CustomLoginSuccessHandler.kt
@@ -1,11 +1,15 @@
 package com.example.project_study.config.session_security
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jsonMapper
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.core.Authentication
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.web.DefaultRedirectStrategy
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler
 import org.springframework.stereotype.Component
+import java.security.Principal
 
 @Component
 class CustomLoginSuccessHandler():AuthenticationSuccessHandler {
@@ -18,10 +22,11 @@ class CustomLoginSuccessHandler():AuthenticationSuccessHandler {
             /*agent 요약: 모바일 web -> android / 어플 -> okhttp*/
             //println(userAgent) //신기한게, 아이폰이랑 맥이랑 똑같이 나옴
 
+
             if(userAgent?.contains("okhttp") == true){
                 //TODO("이게 맞다면, 유저정보 일부를 reponse하도록 만들어주세요")
                 response?.contentType="application/json"
-                response?.writer?.write(authentication?.principal.toString())
+                response?.writer?.write(ObjectMapper().writeValueAsString(authentication?.principal as UserDetails))
             }else{
                 //TODO("이전 페이지로 돌아가도록 만들어주세요")
                 /*급할 거 없이 리팩토링 단계에서 진행하시면 됩니다.*/
@@ -29,5 +34,4 @@ class CustomLoginSuccessHandler():AuthenticationSuccessHandler {
                 redirectStrategy.sendRedirect(request, response, "/")
             }
         }
-
 }
---
+++ src/main/kotlin/com/example/project_study/data/account/AccountDTO.kt
@@ -0,0 +1,5 @@
+package com.example.project_study.data.account
+
+data class AccountDTO(
+    val test:String
+)
