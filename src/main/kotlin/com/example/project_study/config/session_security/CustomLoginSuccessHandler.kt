08534a40c0e22cf4e6002c3180a809df9ae9a727
JorimJoram <wkdgyfla97@gmail.com>
2023-10-26 16:51:39 +0900

    test: response object

--- src/main/kotlin/com/example/project_study/config/session_security/CustomLoginSuccessHandler.kt
+++ src/main/kotlin/com/example/project_study/config/session_security/CustomLoginSuccessHandler.kt
@@ -3,6 +3,7 @@ package com.example.project_study.config.session_security
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.core.Authentication
+import org.springframework.security.web.DefaultRedirectStrategy
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler
 import org.springframework.stereotype.Component
 
@@ -15,15 +16,18 @@ class CustomLoginSuccessHandler():AuthenticationSuccessHandler {
     ) {
             val userAgent = request?.getHeader("User-Agent")
             /*agent 요약: 모바일 web -> android / 어플 -> okhttp*/
-            println(userAgent) //신기한게, 아이폰이랑 맥이랑 똑같이 나옴
+            //println(userAgent) //신기한게, 아이폰이랑 맥이랑 똑같이 나옴
 
-            if(userAgent?.contains("Android") == true){
-                println("Access By Android")
-            }else if (userAgent?.contains("okhttp") == true){
-                println("Access By Application with OkHttp3")
-            }else{
-                println("Access by Other")
+            if(userAgent?.contains("okhttp") == true){
+                //TODO("이게 맞다면, 유저정보 일부를 reponse하도록 만들어주세요")
+                response?.contentType="application/json"
+                response?.writer?.write(authentication?.principal.toString())
             }
+
+            //TODO("이전 페이지로 돌아가도록 만들어주세요")
+            /*급할 거 없이 리팩토링 단계에서 진행하시면 됩니다.*/
+            val redirectStrategy = DefaultRedirectStrategy()
+            redirectStrategy.sendRedirect(request, response, "/")
     }
 
 }
